7b858720d125eecd3d1076825175a578437d8f60
Uwe B <kontakt@uwe-barthel.net>
2019-01-11 17:13:47 +0100

    Update Next beta

--- next.config.js
+++ next.config.js
@@ -1,2 +1,18 @@
 const withSass = require('@zeit/next-sass')
-module.exports = withSass()
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withSass(), {
+            // You may only need to add assetPrefix in the production.
+            assetPrefix: isProd ? 'https://cdn.uwe-barthel.net/cdn/' : ''
+        }, {
+            exportPathMap: async function (defaultPathMap) {
+                return {
+                    '/': { page: '/' },
+                    '/ueber': { page: '/ueber' },
+                    '/portfolio': { page: '/portfolio' },
+                    '/impressum': { page: '/impressum' },
+                    '/datenschutz': { page: '/ueber' },
+                    '/404': { page: '/404' }
+                }
+            }
+        }
